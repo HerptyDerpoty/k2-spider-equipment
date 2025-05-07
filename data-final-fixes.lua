29f5ab3a302155e87745d3633117de333a3859ac
HerptyDerpoty <herptyderpoty@gmail.com>
2025-05-07 18:14:50 -0400

    initial

---
+++ data-final-fixes.lua
@@ -0,0 +1,13 @@
+require("__ylwlib__.utils")
+--add catagory
+data:extend({{ type = "equipment-category", name = "kr-add-spider" }})
+--insert new category in spidertron grid
+table.insert(data.raw["equipment-grid"]["kr-spidertron-equipment-grid"].equipment_categories, "kr-add-spider")
+--add equipment to the new category
+table.insert(data.raw["movement-bonus-equipment"]["exoskeleton-equipment"].categories, "kr-add-spider")
+table.insert(data.raw["movement-bonus-equipment"]["kr-advanced-exoskeleton-equipment"].categories, "kr-add-spider")
+table.insert(data.raw["movement-bonus-equipment"]["kr-superior-exoskeleton-equipment"].categories, "kr-add-spider")
+
+--testing
+
+Remove_value(data.raw["equipment-grid"]["kr-spidertron-equipment-grid"].equipment_categories, {"kr-vehicle-motor"})
